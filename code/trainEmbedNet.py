ad7c090cc565d884c75f2ac4fca9d6adbb7dc2e9
litcoderr <litcoderr@gmail.com>
2025-12-04 16:44:04 +0900

    impl: masking non-facial regions

--- code/trainEmbedNet.py
+++ code/trainEmbedNet.py
@@ -116,10 +116,14 @@ def main_worker(args, wandb_run=None):
 
     ## Input transformations for training (you can change if you like)
     train_transform = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Resize(256),
-         transforms.RandomCrop([224,224]),
-         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+        [
+            transforms.RandomResizedCrop(224, scale=(0.8, 1.0), ratio=(0.9, 1.1)),
+            transforms.RandomHorizontalFlip(),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.05),
+            transforms.RandomGrayscale(p=0.1),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
 
     ## Input transformations for evaluation (you can change if you like)
     test_transform = transforms.Compose(
